276e600d33590c1aeb44aa7d21a4aaf41886b7e9
shakiz <activities335@gmail.com>
2019-12-29 22:22:34 +0600

    basic layout and activity setup

--- app/src/main/java/com/shakil/devtrendz/MainActivity.java
+++ app/src/main/java/com/shakil/devtrendz/MainActivity.java
@@ -1,14 +1,29 @@
 package com.shakil.devtrendz;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
+import com.shakil.devtrendz.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this , R.layout.activity_main);
+
+        init();
+        bindUIWithComponents();
+    }
+
+
+    private void init() {
+
+    }
+
+    private void bindUIWithComponents() {
+
     }
 }
